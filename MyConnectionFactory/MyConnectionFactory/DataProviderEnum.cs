ad6b74703ae1c48ce979a2a8cd44f561b1542dfb
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-05-29 09:37:12 -0400

    Using interfaces to manage db connections

---
+++ MyConnectionFactory/MyConnectionFactory/DataProviderEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConnectionFactory
+{
+  enum DataProviderEnum
+  {
+    // OleDb is windows only and is not supported in .NET Core
+    SqlServer,
+#if PC
+    OleDb,
+#endif
+    Odbc,
+    None
+  }
+}
---
+++ MyConnectionFactory/MyConnectionFactory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.Odbc;
+#if PC
+using System.Data.OleDb;
+#endif
+using Microsoft.Data.SqlClient;
+
+namespace MyConnectionFactory
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("***** Very simple connection factory *****\n");
+      Setup(DataProviderEnum.SqlServer);
+#if PC
+      Setup(DataProviderEnum.OleDb);
+#endif
+      Setup(DataProviderEnum.Odbc);
+      Setup(DataProviderEnum.None);
+    }
+
+    private static void Setup(DataProviderEnum dataProvider)
+    {
+      // Get a specific connection
+      IDbConnection myConnection = GetConnection(dataProvider);
+      Console.WriteLine($"Your connection is a {myConnection?.GetType().Name ?? "unrecognized type"}");
+    }
+
+    private static IDbConnection GetConnection(DataProviderEnum dataProvider) => dataProvider switch
+    {
+      DataProviderEnum.SqlServer => new SqlConnection(),
+#if PC
+      // Not supported on macOS
+      DataProviderEnum.OleDb => new OleDbConnection(),
+#endif
+      DataProviderEnum.Odbc => new OdbcConnection(),
+      _ => null
+    };
+  }
+}
